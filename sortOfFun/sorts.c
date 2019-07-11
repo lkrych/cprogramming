2e91fb18d445c2565c6e41583d4fbc31ccd1944f
Leland Krych <leland.krych@gmail.com>
2019-07-11 14:20:04 -0700

    fix bubble sort by passing length of array as arg to functions

--- sortOfFun/sorts.c
+++ sortOfFun/sorts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h> //include true and false
 
-void print_array(int *arr) {
+// HELPER FUNCTIONS 
+void print_array(int *arr, int length) {
+    // bc arrays are passed as pointers
+    // you need the length of the array to print the entire array
     int i = 0;
-    for (i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+    for (i = 0; i < length; i++) {
         printf("%d, ", arr[i]);
     }
     printf("\n");
@@ -16,19 +19,16 @@ int * swap( int *arr, int i, int j) {
     return arr;
 }
 
+// SORTING ALGORITHMS
+
 // bubble sort
-int * bubble_sort( int *arr){
+int * bubble_sort( int *arr, int length){
     bool sorted = false;
-    int arrSize = 0;
     int i = 0;
-    arrSize = sizeof(arr)/sizeof(int);
-    printf("The size of the array is %d \n", arrSize);
-    printf("The array at begginning of bubble_sort is: ");
-    print_array(arr);
     while (!sorted) {
         sorted = true;
-        for (i = 0; i < arrSize; ++i) {
-            if (i + 1 < arrSize) { //only check for swap up to the second to last element
+        for (i = 0; i < length; ++i) {
+            if (i + 1 < length) { //only check for swap up to the second to last element
                 if (arr[i] > arr[i+1]) {
                     swap(arr, i, i+1);
                     sorted = false;
@@ -44,11 +44,8 @@ int * bubble_sort( int *arr){
 
 int main() {
     int sortThisArray[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    printf("before passing to bubble_sort: ");
-    print_array(sortThisArray);
     int arrSize = sizeof(sortThisArray)/sizeof(int);
-    printf("The size of the array is %d \n", arrSize);
-    int * sorted = bubble_sort(sortThisArray);
+    int * sorted = bubble_sort(sortThisArray, arrSize);
     printf("bubble_sort: ");
-    print_array(sorted);
+    print_array(sorted, arrSize);
 }
