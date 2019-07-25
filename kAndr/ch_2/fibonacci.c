6d61d108ea6b8bfe1f619be067d511da19b61273
Leland Krych <leland.krych@gmail.com>
2019-07-25 10:19:15 -0700

    add fibonacci naive and optimized in c

---
+++ kAndr/ch_2/fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// time for fibonacci of 41:  0m1.074s
+int fibonacci(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+// time for fibonacci_dynamic of 41: 0m0.005s
+// time for fibonacci_dynamic of 1000: 0m0.005s there is definitely overflow the answers are not the same
+int fibonacci_dynamic(n) {
+    int current, new_current, prev_val, i;
+    current = 1;
+    prev_val = 0;
+    i = 0;
+    while (i < n) {
+        new_current = prev_val + current;
+        prev_val = current;
+        current = new_current;
+        i++;
+    }
+    return current;
+}
+
+int main() {
+    int n = 1000;
+    long ans;
+    ans = fibonacci_dynamic(n);
+    printf("Fibonacci of %d is %ld \n", n, ans);
+}
