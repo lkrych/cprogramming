bd223b07e4eb8b03a4a6789b89ff91e64cb29b2a
Leland Krych <leland.krych@gmail.com>
2019-07-30 20:07:45 -0600

    begin scoping out invert in c

---
+++ kAndr/ch_2/invert.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int invert(int x, int p, int n) {
+    
+}
+
+int main() {
+    int ans1,ans2,ans3;
+    ans1 = invert(5, 1, 1);
+    ans2 = invert(8, 2, 2);
+    ans3 = invert(26, 4, 3);
+
+    printf("inverted 5 is %d \n", ans1);
+    printf("inverted 8 is %d \n", ans2);
+    printf("inverted 26 is %d \n", ans3);
+}
