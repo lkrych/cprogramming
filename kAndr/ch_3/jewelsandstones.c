619ed8a96e3cfbeb2eba85cbc51017594446e0dc
Leland Krych <leland.krych@gmail.com>
2019-08-05 08:04:03 -0700

    begin work on j and s c:

---
+++ kAndr/ch_3/jewelsandstones.c
@@ -0,0 +1,51 @@
+// You're given strings J representing the types of stones that are jewels,
+//  and S representing the stones you have.  
+// Each character in S is a type of stone you have.  
+// You want to know how many of the stones you have are also jewels.
+
+// The letters in J are guaranteed distinct, and all characters in J and S are letters. 
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+#include <stdio.h>
+#include <string.h>
+#include "uthash.h"
+
+struct my_dict {
+    char id; // use this as the key
+    char truthy[10];
+    UT_hash_handle hh; // make this structure hashable
+};
+
+struct my_dict *jewels = NULL;
+
+void add_jewel(struct my_dict *s) {
+    HASH_ADD_INT(jewels, id, s);
+}
+
+struct my_dict *find_user(char id) {
+    struct my_dict *s;
+
+    HASH_FIND_INT( jewels, &id, s);
+    return s;
+}
+
+void jewels_and_stones(char *j, int jl, char *s, int sl) {
+
+}
+
+
+int main() {
+    char j1[] = "aA";
+    char s1[] = "aAAbbbb";
+    char j2[] = "z";
+    char s2[] = "ZZ";
+    int j1l,s1l,j2l,s2l;
+    j1l = strlen(j1);
+    j2l = strlen(j2);
+    s1l = strlen(s1);
+    s2l = strlen(s2);
+
+    jewels_and_stones(j1, j1l, s1, s1l);
+    jewels_and_stones(j2, j2l, s2, s2l);
+}
+
