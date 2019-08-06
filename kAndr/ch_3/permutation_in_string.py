3789420a1b8a9c387802587a5e77ccb4981e6c1a
Leland Krych <leland.krych@gmail.com>
2019-08-06 12:40:50 -0700

    add optimized permutation in string

--- kAndr/ch_3/permutation_in_string.py
+++ kAndr/ch_3/permutation_in_string.py
@@ -15,7 +15,7 @@ def permutation(d: List, i: int, l: int, results: List) -> List[str]:
 # Given two strings s1 and s2, write a function to return true 
 # if s2 contains the permutation of s1. 
 # In other words, one of the first string's permutations is the substring of the second string.
-def check_permutation(s1: str, s2: str) -> bool:
+def check_permutation_naive(s1: str, s2: str) -> bool:
     #find perms in s1
     perms = []
     permutation(list(s1), 0, len(s1), perms)
@@ -26,6 +26,34 @@ def check_permutation(s1: str, s2: str) -> bool:
             return True
     return False
 
-assert check_permutation('ab', 'eidbaooo') == True
-assert check_permutation('ab', 'eidboaoo') == False
+assert check_permutation_naive('ab', 'eidbaooo') == True
+assert check_permutation_naive('ab', 'eidboaoo') == False
 
+# a permutation of a word is going to have the same number of characters
+# use an array to store these chars and compare each window in the array
+def check_permutation_optimized(s1: str, s2: str) -> bool:
+    s1_window = create_window(s1)
+    for i in range(len(s2) - len(s1)):
+        check_window = create_window(s2[i:i+len(s1)])
+        if check_window_equality(s1_window, check_window):
+            return True
+    return False
+
+def create_window(s: str) -> List[int]:
+    ALPHA = { 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25 }
+    alpha_arr = [0] * 26
+    
+    for char in s:
+        i = ALPHA[char]
+        alpha_arr[i] += 1
+    
+    return alpha_arr
+
+def check_window_equality(arr1: List[int], arr2: List[int]) -> bool:
+    for i in range(len(arr1)):
+        if arr1[i] != arr2[i]:
+            return False
+    return True
+
+assert check_permutation_optimized('ab', 'eidbaooo') == True
+assert check_permutation_optimized('ab', 'eidboaoo') == False
