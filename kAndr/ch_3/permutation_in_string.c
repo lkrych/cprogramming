50aab047d82777062307065e8bae422b79db6c6b
Leland Krych <leland.krych@gmail.com>
2019-08-09 14:11:21 -0700

    finish permutation in string

--- kAndr/ch_3/permutation_in_string.c
+++ kAndr/ch_3/permutation_in_string.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h> 
+#include <ctype.h> //tolower
 
-ALPHA_LEN = 26;
+int ALPHA_LEN = 26;
 
 void zero(int *arr, int arr_len) {
     int i;
@@ -11,16 +13,33 @@ void zero(int *arr, int arr_len) {
 }
 
 int get_char_index(char c) {
-    return c - '0';
+    int lower; // will only work for lowercase
+    lower = tolower(c); 
+    return lower - 97;
 }
 
 void get_window(char *s, int i, int j, int *arr) {
+    int k,c,c_index;
     int window_len = j - i;
+    for (k = i; k < j; k++) {
+        c = s[k]; // grab char
+        c_index = get_char_index(c);
+        arr[c_index]++;
+    }
+}
 
+int compare_windows(int *w1, int *w2) {
+    int j;
+    for (j = 0; j < ALPHA_LEN; j++) {
+        if (w1[j] != w2[j]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 void find_permutation_in_string(char *s1, int l1, char *s2, int l2) {
-    int i;
+    int i, comp;
     int window1[ALPHA_LEN];
     int window2[ALPHA_LEN];
     get_window(s1, 0, l1, window1);
@@ -30,9 +49,23 @@ void find_permutation_in_string(char *s1, int l1, char *s2, int l2) {
         
         //iterate through all windows and compare windows
         get_window(s2, i, i + l1, window2);
+
+        comp = compare_windows(window1, window2);
+
+        if (comp) {
+            printf("A permutation exists \n");
+        }
     }
 }
 
 int main() {
+    char c1[] = "ab";
+    char s1[] = "eidbaooo";
+    char s2[] = "eidboaoo";
+    int c1l, s1l, s2l;
+    c1l = strlen(c1);
+    s1l = strlen(s1);
+    s2l = strlen(s2);
 
+    find_permutation_in_string(c1, c1l, s2, s2l);
 }
