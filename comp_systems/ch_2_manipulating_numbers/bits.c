dea5b188e91b4e0429d94654584091c8f8ecce20
Leland Krych <leland.krych@gmail.com>
2019-11-30 22:12:01 -0800

    add cmellon lab 1

---
+++ comp_systems/ch_2_manipulating_numbers/bits.c
@@ -0,0 +1,192 @@
+// Introduction to Computing Systems - Data Lab
+//1
+/* 
+ * bitOr - x|y using only ~ and & 
+ *   Example: bitOr(6, 5) = 7
+ *   Legal ops: ~ &
+ *   Max ops: 8
+ *   Rating: 1
+ */
+int bitOr(int x, int y) {
+    /*
+    (x & y): x = 1 and y = 1 yields 1, otherwise 0, 
+  (~x & ~y): x = 0 and y = 0 yields 1, otherwise 0,
+ ~(~x & ~y): x = 0 and y = 0 yields 0, otherwise 1
+    */
+   return ~(~x & ~y)
+}
+/* 
+ * specialBits - return bit pattern 0xffca3fff
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 3
+ *   Rating: 1
+ */
+int specialBits(void) {
+   /*
+   We can't use numbers bigger than 0xFF, 
+   so we need to look for an eight big pattern that we can shift over
+    F    F    C    A    3    F    F    F
+    1111 1111 1100 1010 0011 1111 1111 1111
+    1111 1111 1100 1010 0011 1111 1111 1111
+         
+    The eight bit pattern we are looking for is 0010 1000
+    however, if we shift it over by 14 the most significant bits will all be zero.
+    We need to use a complement operation (~)
+
+    0    0    0    0    0    0    D    7
+    0000 0000 0000 0000 0000 0000 1101 0111
+    0    0    3    5    C    0    0    0
+    0000 0000 0011 0101 1100 0000 0000 0000
+    F    F    C    A    3    F    F    F
+    1111 1111 1100 1010 0011 1111 1111 1111
+
+   */
+
+  return ~(0xD7 << 14)
+}
+//2
+/*
+ * isZero - returns 1 if x == 0, and 0 otherwise 
+ *   Examples: isZero(5) = 0, isZero(0) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 2
+ *   Rating: 1
+ */
+int isZero(int x) {
+    return !x;
+}
+/* 
+ * anyEvenBit - return 1 if any even-numbered bit in word set to 1
+ *   Examples anyEvenBit(0xA) = 0, anyEvenBit(0xE) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 2
+ */
+int anyEvenBit(int x) {
+   /*
+   Use a mask to preserve only even numbered bits
+   0x55 = 85 = 1010101
+   */
+    int mask = 0x55;
+    mask |= (mask << 8);
+    mask |= (mask << 16);
+    !!(x & mask)
+}
+/* 
+ * negate - return -x 
+ *   Example: negate(1) = -1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int negate(int x) {
+   return ~x + 1
+}
+/* 
+ * leastBitPos - return a mask that marks the position of the
+ *               least significant 1 bit. If x == 0, return 0
+ *   Example: leastBitPos(96) = 0x20
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2 
+ */
+int leastBitPos(int x) {
+    
+}
+//3
+/* 
+ * rotateLeft - Rotate x to the left by n
+ *   Can assume that 0 <= n <= 31
+ *   Examples: rotateLeft(0x87654321,4) = 0x76543218
+ *   Legal ops: ~ & ^ | + << >> !
+ *   Max ops: 25
+ *   Rating: 3 
+ */
+int rotateLeft(int x, int n) {
+    
+}
+/* 
+ * divpwr2 - Compute x/(2^n), for 0 <= n <= 30
+ *  Round toward zero
+ *   Examples: divpwr2(15,1) = 7, divpwr2(-33,4) = -2
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 2
+ */
+int divpwr2(int x, int n) {
+    
+}
+/* 
+ * isLess - if x < y  then return 1, else return 0 
+ *   Example: isLess(4,5) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 24
+ *   Rating: 3
+ */
+int isLess(int x, int y) {
+    
+}
+//4
+/*
+ * isPower2 - returns 1 if x is a power of 2, and 0 otherwise
+ *   Examples: isPower2(5) = 0, isPower2(8) = 1, isPower2(0) = 0
+ *   Note that no negative number is a power of 2.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int isPower2(int x) {
+    
+}
+/*
+ * bitReverse - Reverse bits in a 32-bit word
+ *   Examples: bitReverse(0x80000002) = 0x40000001
+ *             bitReverse(0x89ABCDEF) = 0xF7D3D591
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 45
+ *   Rating: 4
+ */
+int bitReverse(int x) {
+    
+}
+//float
+/* 
+ * float_abs - Return bit-level equivalent of absolute value of f for
+ *   floating point argument f.
+ *   Both the argument and result are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representations of
+ *   single-precision floating point values.
+ *   When argument is NaN, return argument..
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 10
+ *   Rating: 2
+ */
+unsigned float_abs(unsigned uf) {
+    
+}
+/* 
+ * float_i2f - Return bit-level equivalent of expression (float) x
+ *   Result is returned as unsigned int, but
+ *   it is to be interpreted as the bit-level representation of a
+ *   single-precision floating point values.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+unsigned float_i2f(int x) {
+   
+}
+/* 
+ * float_times64 - Return bit-level equivalent of expression 64*f for
+ *   floating point argument f.
+ *   Both the argument and result are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representation of
+ *   single-precision floating point values.
+ *   When argument is NaN, return argument
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 35
+ *   Rating: 4
+ */
+unsigned float_times64(unsigned uf) {
+    
+}
